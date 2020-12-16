1e61c36dad0a5f5cbbc4269e157471fa6fa741c5
Victor Aremu <victor.olorunbunmi@gmail.com>
2020-12-17 00:51:56 +0100

    Implement useSelection hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,92 @@
-import * as React from 'react'
+import {
+  useState,
+  useRef,
+  Dispatch,
+  SetStateAction,
+  RefObject,
+  ChangeEvent
+} from 'react'
 
-interface Props {
-  text: string
+export type SetSelected = Dispatch<SetStateAction<number[]>>
+export type ItemsListContainerRef = RefObject<HTMLElement>
+export type OnChange = ChangeEvent<HTMLInputElement>
+
+const methods = {
+  selectItem(index: number, selectedItems: number[], setSelected: SetSelected) {
+    const nextState = [...selectedItems, index]
+
+    setSelected(nextState)
+  },
+
+  unSelectItems(
+    listOfIndex: number[],
+    selectedItems: number[],
+    setSelected: SetSelected
+  ) {
+    setSelected(selectedItems.filter((index) => !listOfIndex.includes(index)))
+  },
+
+  toggleSelect(
+    event: OnChange,
+    index: number,
+    selectedItems: number[],
+    setSelected: SetSelected
+  ) {
+    event.target.checked
+      ? methods.selectItem(index, selectedItems, setSelected)
+      : methods.unSelectItems([index], selectedItems, setSelected)
+  },
+
+  toggleSelectAll(
+    event: OnChange,
+    itemsListContainerRef: ItemsListContainerRef,
+    itemsCount: number,
+    selectedItems: number[],
+    setSelected: SetSelected
+  ) {
+    const listOfIndex = new Array(itemsCount).fill(null).map((_x, i) => i)
+
+    event.target.checked
+      ? setSelected(listOfIndex)
+      : methods.unSelectItems(listOfIndex, selectedItems, setSelected)
+
+    if (itemsListContainerRef.current) {
+      const checkboxes: NodeListOf<Element> = itemsListContainerRef.current.querySelectorAll(
+        'input[type=checkbox]'
+      )
+
+      checkboxes.forEach((checkbox) => {
+        ;(checkbox as HTMLInputElement).checked = event.target.checked
+      })
+    }
+  }
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <>Example Component: {text}</>
+const useSelection = () => {
+  const itemsListContainerRef = useRef<HTMLElement>(null)
+
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  return {
+    itemsListContainerRef,
+
+    selectedItems,
+    setSelectedItems,
+
+    handleToggleSelect(event: OnChange, index: number) {
+      methods.toggleSelect(event, index, selectedItems, setSelectedItems)
+    },
+
+    handleToggleSelectAll(event: OnChange, itemsCount: number) {
+      methods.toggleSelectAll(
+        event,
+        itemsListContainerRef,
+        itemsCount,
+        selectedItems,
+        setSelectedItems
+      )
+    }
+  }
 }
+
+export default useSelection
